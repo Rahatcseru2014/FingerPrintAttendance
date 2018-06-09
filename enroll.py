710de2d86f02b7ac612a1f3e59b38cfc1ef32139
Rahat Linkon <rahat.javafx@gmail.com>
2018-06-09 11:59:01 +0000

    fingerprint scan added

--- enroll.py
+++ enroll.py
@@ -1,12 +1,38 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QDialog
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
 from PyQt5.uic import loadUi
+from fingerprint_enroll import FingerPrintScan
 
 
 class Ui_Enroll(QDialog):
     def __init__(self):
         super(Ui_Enroll, self).__init__()
         loadUi('enroll.ui', self)
+        self.pushButton.clicked.connect(self.on_scan_click)        
+
+    
+    def on_scan_click(self):
+        try:
+            self.finger_print_module = FingerPrintScan()
+            self.finger_print_module.scan_fingerprint()
+            self.finger_print_module.save_as_image(self.lineEdit.text())
+            self.finger_print_module.save_to_char_buffer()
+            if(self.finger_print_module.is_fingerprint_exist()):
+                QMessageBox.about(self,'Error!','Fingerprint already exist!')
+                return
+            
+            scene = QGraphicsScene()
+            scene.addPixmap(QPixmap(self.lineEdit.text()+'.png'))
+            self.graphicsView.setScene(scene)
+            
+            self.finger_print_module.save_fingerprint()
+            if(self.finger_print_module.is_fingerprint_exist()):
+                QMessageBox.about(self,'Saved','Fingerprint stored successfully!')
+                return
+        except Exception as e:
+            print('The fingerprint sensor could not be initialized!')
+            print('Exception message: ' + str(e))
 
 
 if __name__ == "__main__":
---
+++ fingerprint_enroll.py
@@ -0,0 +1,48 @@
+import time
+from pyfingerprint import PyFingerprint
+import sqlite3
+class FingerPrintScan:
+    
+    def __init__(self):        
+        self.f = PyFingerprint('/dev/ttyUSB0', 57600, 0xFFFFFFFF, 0x00000000)
+        if ( self.f.verifyPassword() == False ):
+            raise ValueError('The given fingerprint sensor password is wrong!')
+        print('Currently used templates: ' + str(self.f.getTemplateCount()) +'/'+ str(self.f.getStorageCapacity()))
+        self.current_buffer = 0x01
+    
+    def scan_fingerprint(self):
+        while ( self.f.readImage() == False ):
+            pass
+    
+    def is_fingerprint_exist(self):
+        result = self.f.searchTemplate()
+        positionNumber = result[0]
+        print(positionNumber)
+        return positionNumber >= 0
+    
+    def get_template_id(self):
+        esult = self.f.searchTemplate()
+        positionNumber = result[0]
+        print(positionNumber)
+        return positionNumber
+    
+    def save_fingerprint(self):
+        self.f.createTemplate()
+        return self.f.storeTemplate()
+
+    def save_to_char_buffer(self):
+        self.f.convertImage(self.current_buffer)
+        self.current_buffer = self.current_buffer + 1
+    
+    def save_as_image(self,file_name):
+        imageDestination =  '/home/pi/Downloads/FingerPrintAttendance/'+file_name+'.png'
+        self.f.downloadImage(imageDestination)                
+        
+if __name__ == "__main__":
+    try:
+        finger_print_module = FingerPrintScan()
+        finger_print_module.scan_fingerprint()
+        finger_print_module.save_as_image('Linkon')
+    except Exception as e:
+        print('The fingerprint sensor could not be initialized!')
+        print('Exception message: ' + str(e))
