404aa58b11d0c48281bba3a25414a50d8c31f2bb
Linkon <rahat.javafx@gmail.com>
2018-06-09 18:04:30 +0600

    file intended correct;y

--- enroll.py
+++ enroll.py
@@ -9,26 +9,25 @@ class Ui_Enroll(QDialog):
     def __init__(self):
         super(Ui_Enroll, self).__init__()
         loadUi('enroll.ui', self)
-        self.pushButton.clicked.connect(self.on_scan_click)        
+        self.pushButton.clicked.connect(self.on_scan_click)
 
-    
     def on_scan_click(self):
         try:
             self.finger_print_module = FingerPrintScan()
             self.finger_print_module.scan_fingerprint()
             self.finger_print_module.save_as_image(self.lineEdit.text())
             self.finger_print_module.save_to_char_buffer()
-            if(self.finger_print_module.is_fingerprint_exist()):
-                QMessageBox.about(self,'Error!','Fingerprint already exist!')
+            if (self.finger_print_module.is_fingerprint_exist()):
+                QMessageBox.about(self, 'Error!', 'Fingerprint already exist!')
                 return
-            
+
             scene = QGraphicsScene()
-            scene.addPixmap(QPixmap(self.lineEdit.text()+'.png'))
+            scene.addPixmap(QPixmap(self.lineEdit.text() + '.png'))
             self.graphicsView.setScene(scene)
-            
+
             self.finger_print_module.save_fingerprint()
-            if(self.finger_print_module.is_fingerprint_exist()):
-                QMessageBox.about(self,'Saved','Fingerprint stored successfully!')
+            if self.finger_print_module.is_fingerprint_exist():
+                QMessageBox.about(self, 'Saved', 'Fingerprint stored successfully!')
                 return
         except Exception as e:
             print('The fingerprint sensor could not be initialized!')
