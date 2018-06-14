f7fc885c56d9d6f2340e3c99b0746608390f91a3
Rahat Rafiq <36023339+Rahatcseru2014@users.noreply.github.com>
2018-06-14 12:29:04 +0600

    Update pyfingerprint.py

--- pyfingerprint.py
+++ pyfingerprint.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-PyFingerprint
-Copyright (C) 2015 Bastian Raschke <bastian.raschke@posteo.de>
-All rights reserved.
-
-"""
 
 import os
 import serial
